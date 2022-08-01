4b99b7b25c7604b435e0350f06074df7690cbef1
Denis Istratenko <denis.istratenko@netcracker.com>
2022-08-01 16:02:35 +0300

    first commit

---
+++ src/main/java/org/test/openapi/EntityRestApi.java
@@ -0,0 +1,24 @@
+package org.test.openapi;
+
+import org.test.openapi.rest.EntityApi;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import java.util.UUID;
+
+@Path("/api/private") //java.lang.StackOverflowError
+//@Path("/hello") //successful build
+//@Path("/hello/quarkus") //successful build
+public class EntityRestApi implements EntityApi {
+
+    @POST
+    @Path("/entityset/perform")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Override
+    public String perform(@QueryParam("id") final UUID id) {
+        return "response";
+    }
+}
